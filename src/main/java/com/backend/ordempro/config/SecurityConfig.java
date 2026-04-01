b10b7482afdcc4ed4c6af071de5d0e1b9aae6e15
albadauto <joseadauto923@gmail.com>
2026-04-01 18:12:01 -0300

    Cors with prod

--- src/main/java/com/backend/ordempro/config/SecurityConfig.java
+++ src/main/java/com/backend/ordempro/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.backend.ordempro.filters.JwtFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -17,6 +18,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/admin/**").authenticated()
                         .requestMatchers("/customer/**").authenticated()
                         .requestMatchers("/serviceorder/**").authenticated()
